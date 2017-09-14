55afab0eb76f40f4d6045b882363c7338a9eebee
Goran Gajic <fero.null@gmail.com>
2017-09-14 11:57:29 +0200

    change build

--- build.sh
+++ build.sh
@@ -2,4 +2,3 @@
 webpack=node_modules/.bin/webpack
 NODE_ENV=production $webpack lib/retinable.js dist/retinable.js
 NODE_ENV=production COMPRESS=1 $webpack lib/retinable.js dist/retinable.min.js
-npm version patch -m "Upgrade to %s"
